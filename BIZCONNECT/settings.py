ba95b95542978ff12ecbbab982a2b36dda1a6224
Athulya2507 <nayanadominic25@gmail.com>
2026-02-06 22:56:22 +0530

    Update settings.py
    upated secret key

--- BIZCONNECT/settings.py
+++ BIZCONNECT/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sknp-s(o_r(*ye#)(f^bwvwcx4u!pys7_zr!-wul%y@mk4j#t+'
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
